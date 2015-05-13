612f377593e7d8818f8ac1914fac95e4d6c78171
Paul Maseberg <pmaseberg@accuweather.com>
2015-05-13 11:11:46 -0500

    Adding a bearing and direction function for points.
    Already had a distance function so I just added a bearing and direction
    function.

--- point.go
+++ point.go
@@ -42,14 +42,55 @@ func (p *GeoPoint) Lon() float64 {
 	return p.Plon
 }
 
+func Direction(p1, p2 Point) string {
+
+	bearingList := []string{"NE", "E", "SE", "S", "SW", "W", "NW", "N"}
+
+	bearing := Bearing(p1, p2)
+
+	index := bearing - 22.5
+
+	if index < 0 {
+		index += 360
+	}
+	indexInt := int(index / 45.0)
+
+	return bearingList[indexInt]
+}
+
+func Bearing(p1, p2 Point) float64 {
+
+	// lamda LON
+	// phi   LAT
+
+	lat1 := toRadians(p1.Lat())
+	lon1 := toRadians(p1.Lon())
+
+	lat2 := toRadians(p2.Lat())
+	lon2 := toRadians(p2.Lon())
+
+	y := math.Sin(lon2-lon1) * math.Cos(lat2)
+	x := math.Cos(lat1)*math.Sin(lat2) -
+		math.Sin(lat1)*math.Cos(lat2)*math.Cos(lon2-lon1)
+
+	bearing := math.Atan2(y, x)
+
+	return float64(int(toDegrees(bearing) + 180%360))
+}
+
 func Distance(p1, p2 Point) Meters {
 	return distance(p1, p2)
 }
 
+func toDegrees(x float64) float64 {
+	return x * 180.0 / math.Pi
+}
+
+func toRadians(x float64) float64 {
+	return x * math.Pi / 180.0
+}
+
 func distance(p1, p2 Point) Meters {
-	toRadians := func(x float64) float64 {
-		return x * math.Pi / 180.0
-	}
 
 	dLat := toRadians(p2.Lat() - p1.Lat())
 	dLng := toRadians(p2.Lon() - p1.Lon())
