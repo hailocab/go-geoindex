aaf8bb33723f8eb86370651a611fb117e3547aad
Daniel Cannon <daniel@danielcannon.co.uk>
2015-11-30 11:36:01 +0000

    Added tests

--- point_test.go
+++ point_test.go
@@ -1,9 +1,10 @@
 package geoindex
 
 import (
-	"github.com/stretchr/testify/assert"
 	"math"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestDistance(t *testing.T) {
@@ -17,4 +18,21 @@ func TestDistance(t *testing.T) {
 
 	d = math.Sqrt(float64(approximateSquareDistance(oxford, embankment)))
 	assert.InDelta(t, d, 1593.763, 0.001)
+}
+
+func TestDirection(t *testing.T) {
+	assert.Equal(t, North, DirectionTo(waterloo, kingsCross))
+	assert.Equal(t, NorthEast, DirectionTo(leicester, coventGarden))
+	assert.Equal(t, SouthEast, DirectionTo(oxford, embankment))
+}
+
+func TestBearing(t *testing.T) {
+	b := BearingTo(waterloo, kingsCross)
+	assert.InDelta(t, b, -12.659, 0.001)
+
+	b = BearingTo(leicester, coventGarden)
+	assert.InDelta(t, b, 57.706, 0.001)
+
+	b = BearingTo(oxford, embankment)
+	assert.InDelta(t, b, 122.939, 0.001)
 }
